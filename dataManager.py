45ebb5725599126000f6c48d95f79d46bde490de
navin <navin1039@gmail.com>
2026-05-27 19:41:16 +0800

    updated to task on 26/5/26

---
+++ dataManager.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import json
+
+class Datamanager:
+    def __init__(self):
+        self.path = "./data.json"
+        self.cache = []
+        
+        file_path = Path(self.path)
+        # check if ./data.json exists
+        if file_path.exists():
+            # if it exist, read it and save it to self.cache
+            print("path exist")
+            with open(self.path, 'r') as file:
+                self.cache = json.load(file)
+        else: 
+            # create it if it does not
+            self.cache = []
+            self.saveToFile()
+            print('data.json created')
+    #CRUD
+    def createTodo(self, dataSet):
+        # adds the data into the json list
+        self.cache.append(dataSet)
+        # saves the data to localstorage
+        self.saveToFile()
+        
+    def getAllTodo(self):
+        # returns the entire list
+        return self.cache
+
+    def updateOneTodo(self, index, data):
+        # replaces one of the data in the list by index
+        if 0 <= index < len(self.cache):
+            self.cache[index] = data
+            self.saveToFile()
+        else:
+            print("invalid index")
+
+    def deleteOneTodo(self, index):
+        # deletes one of the data in the list by index
+        if 0 <= index < len(self.cache):
+            del self.cache[index]
+            self.saveToFile()
+        else:
+            print("invalid index")
+            
+    def saveToFile(self):
+        with open(self.path, 'w') as file:
+            json.dump(self.cache, file, indent=4)
+        print('data saved to file')
--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from fastapi import FastAPI
-#remember to activate .venv each time i load VSCodium
+from fastapi import FastAPI, Request
+from dataManager import Datamanager
+#remember to activate .venv and launch fastapi dev each time i load VSCodium 
     #source .venv/bin/activate
+    #fastapi dev
 
 server = FastAPI()
+dm = Datamanager()
 
 @server.get('/')
 def root():
     return {"message":"Hello World"}
+
+@server.get('/getAllTodo')
+def getAllTodo():
+    return 'initbruv'
+
+@server.post('/createTodo')
+async def createTodo(req:Request):
+    data = await req.json()
+    dm.createTodo(data)
+    return {'status':'ok'}
+
+@server.delete('/deleteOneTodo/{index}')
+def deleteOneTodo(index:int):
+    dm.deleteOneTodo(index)
+    return {'status':'ok'} 
+
+@server.put('/updateOneTodo/{index}')
+async def UpdateOneTodo(index:int, req:Request):
+    currentData = await req.json()
+    dm.updateOneTodo(index, currentData)
+    return {'status':'ok'} 
+
